20670681ca356f56f5c8881bb02f74390e5cd1e7
huann <huann@grinnell.edu>
2013-10-01 12:02:37 -0500

    printExtremes works with sampleList

--- src/edu/grinnell/csc207/huann/hw5/PrintIncidentExperiment.java
+++ src/edu/grinnell/csc207/huann/hw5/PrintIncidentExperiment.java
@@ -1,28 +1,38 @@
 package edu.grinnell.csc207.huann.hw5;
+
 import edu.grinnell.glimmer.ushahidi.UshahidiClient;
+import edu.grinnell.glimmer.ushahidi.UshahidiLocation;
 import edu.grinnell.glimmer.ushahidi.UshahidiWebClient;
 import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 import edu.grinnell.glimmer.ushahidi.UshahidiIncident;
 import edu.grinnell.glimmer.ushahidi.UshahidiUtils;
 
-
 public class PrintIncidentExperiment {
 	public static void main(String[] args) {
-        // A few basic incidents
-        UshahidiExtensions.printIncident(UshahidiUtils.SAMPLE_INCIDENT);
-        UshahidiExtensions.printIncident(UshahidiUtils.randomIncident());
-        UshahidiExtensions.printIncident(UshahidiUtils.randomIncident());
+		Calendar date = new GregorianCalendar(10, 25, 2013);
+		UshahidiLocation Grinnell = new UshahidiLocation(100, "Grinnell");
+		
+	    // Create the output device
+	    PrintWriter pen = new PrintWriter(System.out, true);
+	    
+	    // A few basic incidents
+	    UshahidiExtensions.printIncident(pen, UshahidiUtils.SAMPLE_INCIDENT);
+	    UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
+	    UshahidiExtensions.printIncident(pen, UshahidiUtils.randomIncident());
 
-        // A newly created incident
-        UshahidiIncident myIncident = new UshahidiIncident(...);
-        UshahidiExtensions.printIncident(myIncident);
+	    // A newly created incident
+	    UshahidiIncident myIncident = new UshahidiIncident(20, "tornado drill", date, Grinnell, "It's a drill");
+	    UshahidiExtensions.printIncident(pen, myIncident);
 
-        // One from a list
-        UshahidiClient client = UshahidiUtils.SAMPLE_CLIENT;
-        UshahidiExtensions.printIncident(client.nextIncident());
+	    // One from a list
+	    UshahidiClient client = UshahidiUtils.SAMPLE_CLIENT;
+	    UshahidiExtensions.printIncident(pen, client.nextIncident());
 
-        // One that requires connecting to the server
-        UshahidiClient webclient = new UshahidiWebClient("https://farmersmarket.crowdmap.com/");
-        UshahidiExtensions.printIncident(webclient.nextIncident());
-    } // main(String[])
+	    // One that requires connecting to the server
+	    UshahidiClient webclient = new UshahidiWebClient("www.burgermap.org");
+	    UshahidiExtensions.printIncident(pen, webclient.nextIncident());
+	} // main(String[])
 }
--- src/edu/grinnell/csc207/huann/hw5/UshahidiExtensions.java
+++ src/edu/grinnell/csc207/huann/hw5/UshahidiExtensions.java
@@ -8,32 +8,43 @@ import edu.grinnell.glimmer.ushahidi.*;
 
 public class UshahidiExtensions {
 
-	public static void printIncident(UshahidiIncident sampleIncident) {
-		PrintWriter pen = new PrintWriter(System.out, true);
-		pen.println("Incident #: " + sampleIncident.getTitle());
-		pen.println(sampleIncident.getDescription());
-		pen.println("Location: " + sampleIncident.getLocation());
-		pen.println("Status: (" + sampleIncident.getMode() + ", "
+	/**
+	 * Prints an incident using a specified format. 
+	 * @param pen
+	 * @param sampleIncident
+	 */
+	public static void printIncident(PrintWriter pen,
+			UshahidiIncident sampleIncident) {
+		pen.println("Incident #: " + sampleIncident.getId() + " "
+				+ sampleIncident.getTitle());
+		pen.println("  " + sampleIncident.getDescription());
+		pen.println("  Location: " + sampleIncident.getLocation());
+		pen.println("  Status: (" + sampleIncident.getMode() + ", "
 				+ sampleIncident.getActive() + ", "
 				+ sampleIncident.getVerified() + ")");
 	} // printIncident(UshahidiIncident)
 
-	static UshahidiLocation Grinnell = new UshahidiLocation(0, "Grinnell");
-	static UshahidiLocation OtherPlace = new UshahidiLocation(0, "Alabama");
-
+	
+	/**
+	 * Creates a list of made-up incidents.
+	 * @return
+	 */
 	public static UshahidiIncidentList createIncidentList() {
-		Calendar calendar0 = new GregorianCalendar(10, 12, 2012);
-		Calendar calendar1 = new GregorianCalendar(7, 5, 2000);
+		Calendar calendar0 = new GregorianCalendar(10, 12, 1979);
+		Calendar calendar1 = new GregorianCalendar(7, 5, 1992);
 		Calendar calendar2 = new GregorianCalendar(2, 4, 1996);
-		Calendar calendar3 = new GregorianCalendar(12, 27, 1991);
-		Calendar calendar4 = new GregorianCalendar(3, 4, 2008);
-		Calendar calendar5 = new GregorianCalendar(8, 10, 2001);
-		Calendar calendar6 = new GregorianCalendar(1, 2, 2002);
+		Calendar calendar3 = new GregorianCalendar(12, 27, 1999);
+		Calendar calendar4 = new GregorianCalendar(3, 4, 2001);
+		Calendar calendar5 = new GregorianCalendar(8, 10, 2002);
+		Calendar calendar6 = new GregorianCalendar(1, 2, 2003);
 		Calendar calendar7 = new GregorianCalendar(7, 9, 2003);
 		Calendar calendar8 = new GregorianCalendar(6, 18, 2004);
 		Calendar calendar9 = new GregorianCalendar(3, 16, 2005);
 		Calendar calendar10 = new GregorianCalendar(9, 14, 2006);
 		Calendar calendar11 = new GregorianCalendar(4, 12, 2007);
+
+		UshahidiLocation Grinnell = new UshahidiLocation(100, "Grinnell");
+		UshahidiLocation OtherPlace = new UshahidiLocation(200, "Alabama");
 
 		UshahidiIncidentList samplelist = new UshahidiIncidentList();
 
@@ -47,19 +58,19 @@ public class UshahidiExtensions {
 				Grinnell, "Hug line!");
 		UshahidiIncident Incident4 = new UshahidiIncident(4, "unicorns",
 				calendar4, Grinnell, "Rare beast sighting");
-		UshahidiIncident Incident5 = new UshahidiIncident(99, "car accident",
+		UshahidiIncident Incident5 = new UshahidiIncident(5, "car accident",
 				calendar5, OtherPlace, "Oops");
-		UshahidiIncident Incident6 = new UshahidiIncident(5,
+		UshahidiIncident Incident6 = new UshahidiIncident(6,
 				"drunk first year", calendar6, Grinnell, "Oops");
-		UshahidiIncident Incident7 = new UshahidiIncident(6, "Meteor strike",
+		UshahidiIncident Incident7 = new UshahidiIncident(7, "Meteor strike",
 				calendar7, OtherPlace, "Pew Pew");
-		UshahidiIncident Incident8 = new UshahidiIncident(7, "Dropped Tray",
+		UshahidiIncident Incident8 = new UshahidiIncident(8, "Dropped Tray",
 				calendar8, Grinnell, "Everybody's looking!");
-		UshahidiIncident Incident9 = new UshahidiIncident(8, "Sinkhole",
+		UshahidiIncident Incident9 = new UshahidiIncident(9, "Sinkhole",
 				calendar9, OtherPlace, "Blorp");
-		UshahidiIncident Incident10 = new UshahidiIncident(9, "Brisco Warning",
-				calendar10, Grinnell, "Attention:");
-		UshahidiIncident Incident11 = new UshahidiIncident(10,
+		UshahidiIncident Incident10 = new UshahidiIncident(10,
+				"Brisco Warning", calendar10, Grinnell, "Attention: ");
+		UshahidiIncident Incident11 = new UshahidiIncident(99,
 				"Nuclear Detonation", calendar11, OtherPlace, "Ka-pew");
 
 		samplelist.addIncident(Incident0);
@@ -78,43 +89,65 @@ public class UshahidiExtensions {
 		return samplelist;
 	} // createIncidentList()
 
+	
+	/**
+	 * Prints the incidents with the lowest and the highest id.
+	 * @param client
+	 * @throws Exception
+	 */
 	public static void printExtremes(UshahidiClient client) throws Exception {
 		PrintWriter pen = new PrintWriter(System.out, true);
-		// UshahidiClient client = new UshahidiWebClient(
-		// "https://farmersmarket.crowdmap.com/");
-		UshahidiIncident LowId = new UshahidiIncident();
-		UshahidiIncident HighId = new UshahidiIncident();
-		UshahidiIncident CurrentIncident = new UshahidiIncident();
+		UshahidiIncident CurrentIncident;
+		UshahidiIncident LowId = client.nextIncident();
+		UshahidiIncident HighId = client.nextIncident();
+
 		while (client.hasMoreIncidents()) {
 			CurrentIncident = client.nextIncident();
-			if (client.nextIncident().getId() > HighId.getId())
+			int nextId = CurrentIncident.getId();
+			if (nextId > HighId.getId()) {
 				HighId = CurrentIncident;
-			if (client.nextIncident().getId() < LowId.getId())
+			} // if
+			if (nextId < LowId.getId()) {
 				LowId = CurrentIncident;
-		}
-		pen.println(HighId);
-		pen.println(LowId);
+			} // if
+		} // while
+		printIncident(pen, HighId);
+		printIncident(pen, LowId);
 	} // printExtremes()
 
+	
+	/**
+	 * Prints all incidents that happen between a given start date and end date. 
+	 * @param client
+	 * @param StartDate
+	 * @param EndDate
+	 * @throws Exception
+	 */
 	public static void printByDate(UshahidiClient client, Calendar StartDate,
 			Calendar EndDate) throws Exception {
 		PrintWriter pen = new PrintWriter(System.out, true);
-		// UshahidiClient client = new UshahidiWebClient(
-		// "https://farmersmarket.crowdmap.com/");
 		UshahidiIncident CurrentIncident = new UshahidiIncident();
+		
 		while (client.hasMoreIncidents()) {
 			CurrentIncident = client.nextIncident();
-			if ((client.nextIncident().getDate().after(StartDate) && client
-					.nextIncident().getDate().before(EndDate))) {
+			Calendar nextDate = CurrentIncident.getDate();
+			if ((nextDate.after(StartDate) && nextDate.before(EndDate))) {
 				pen.println(CurrentIncident);
-			}
-		}
+			} // if
+		} // while
 	} // printByDate(Calendar, Calendar)
 
+	
+	/**
+	 * Creates and returns an array of all incidents that occur between a given start date and end date.
+	 * @param client
+	 * @param StartDate
+	 * @param EndDate
+	 * @return
+	 * @throws Exception
+	 */
 	public static UshahidiIncident[] createByDate(UshahidiClient client,
 			Calendar StartDate, Calendar EndDate) throws Exception {
-		// UshahidiClient client = new UshahidiWebClient(
-		// "https://farmersmarket.crowdmap.com/");
 		UshahidiIncident[] original = client.getIncidents();
 
 		int count = 0;
@@ -137,13 +170,16 @@ public class UshahidiExtensions {
 		return results;
 	} // createByDate(Calendar, Calendar)
 
+	
 	public static void main(String[] args) throws Exception {
 		UshahidiIncidentList samplelist = createIncidentList();
-		Calendar calendar1 = new GregorianCalendar(7, 5, 2000);
+		Calendar calendar1 = new GregorianCalendar(7, 5, 1992);
 		Calendar calendar9 = new GregorianCalendar(3, 16, 2005);
+		//UshahidiClient client = new UshahidiWebClient("www.burgermap.org");
 
 		printExtremes(samplelist);
-		printByDate(samplelist, calendar1, calendar9);
-		createByDate(samplelist, calendar1, calendar9);
+		//printExtremes(client);
+		//printByDate(samplelist, calendar1, calendar9);
+		//createByDate(samplelist, calendar1, calendar9);
 	}
 }
